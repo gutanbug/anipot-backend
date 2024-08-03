db6ca47844d43352b9c4ae62a41b49ec5bf396d9
ChaHyeonMin <gutanbug53@gmail.com>
2024-08-03 20:16:32 +0900

    feat : 스프링 Security Config 추가

--- src/main/java/org/anipotbackend/global/config/SecurityConfig.java
+++ src/main/java/org/anipotbackend/global/config/SecurityConfig.java
@@ -1,2 +1,73 @@
-package org.anipotbackend.global.config;public class SecurityConfig {
+package org.anipotbackend.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.anipotbackend.global.auth.JwtAuthenticationFilter;
+import org.anipotbackend.global.error.CustomAccessDeniedHandler;
+import org.anipotbackend.global.error.CustomAuthenticationEntryPoint;
+import org.anipotbackend.global.error.ExceptionalHandlerFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.firewall.StrictHttpFirewall;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+@EnableMethodSecurity
+public class SecurityConfig {
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final ExceptionalHandlerFilter exceptionalHandlerFilter;
+    private final CustomAccessDeniedHandler customAccessDeniedHandler;
+    private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
+
+    private static final String[] PUBLIC_URI = {
+            "/swagger-ui/**", "/api-docs/**", "/test/**", "/**"
+    };
+
+    private static final String[] ADMIN_URI = {
+            "/admin/**", "/manage/**"
+    };
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(
+                        req -> req
+                                .requestMatchers(PUBLIC_URI).permitAll()
+                                .requestMatchers(ADMIN_URI).hasRole("ADMIN")
+                                .anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(exceptionalHandlerFilter, JwtAuthenticationFilter.class)
+                .exceptionHandling(
+                        handling ->
+                                handling.accessDeniedHandler(customAccessDeniedHandler)
+                                        .authenticationEntryPoint(customAuthenticationEntryPoint)
+                )
+                .build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        StrictHttpFirewall firewall = new StrictHttpFirewall();
+        firewall.setAllowUrlEncodedSlash(true);
+        return webSecurity -> webSecurity.httpFirewall(firewall);
+    }
 }
