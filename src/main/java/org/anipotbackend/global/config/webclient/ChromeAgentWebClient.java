caaf49b24e2259c3bba88af49b015cbbe1d76623
ChaHyeonMin <gutanbug53@gmail.com>
2024-08-03 20:15:26 +0900

    feat : lombok 설정을 위한 WebClient, ChromeAgent 추가

--- src/main/java/org/anipotbackend/global/config/webclient/ChromeAgentWebClient.java
+++ src/main/java/org/anipotbackend/global/config/webclient/ChromeAgentWebClient.java
@@ -1,2 +1,12 @@
-package org.anipotbackend.global.config.webclient;public @interface ChromeAgentWebClient {
+package org.anipotbackend.global.config.webclient;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.TYPE, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Qualifier("chromeAgentWebClient")
+public @interface ChromeAgentWebClient {
 }
