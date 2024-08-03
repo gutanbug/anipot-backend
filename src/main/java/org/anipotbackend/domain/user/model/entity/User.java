95d283f8d68ba9bfc3d9303e950e7455e9fb36cf
ChaHyeonMin <gutanbug53@gmail.com>
2024-08-03 23:25:55 +0900

    fix : 사용자 엔티티 및 상태 enum 클래스 수정

--- src/main/java/org/anipotbackend/domain/user/model/entity/User.java
+++ src/main/java/org/anipotbackend/domain/user/model/entity/User.java
@@ -1,2 +1,56 @@
-package org.anipotbackend.domain.user.model.entity;public class User {
+package org.anipotbackend.domain.user.model.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.anipotbackend.domain.user.model.UserStatus;
+import org.anipotbackend.global.auth.UserRole;
+import org.anipotbackend.global.base.BaseEntity;
+
+@Table
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseEntity {
+
+    private String loginId;
+
+    private String password;
+
+    private String email;
+
+    private String nickname;
+
+    private String phone;
+
+    private String birthDate;
+
+    private UserRole userRole;
+
+    private UserStatus userStatus;
+
+    @Builder
+    private User(@NotNull String loginId,
+                @NotNull String password,
+                @NotNull String email,
+                @NotNull String nickname,
+                @NotNull String phone,
+                @NotNull String birthDate) {
+        this.loginId = loginId;
+        this.password = password;
+        this.email = email;
+        this.nickname = nickname;
+        this.phone = phone;
+        this.userRole = UserRole.USER;
+        this.userStatus = UserStatus.ACTIVE;
+        this.birthDate = birthDate;
+    }
+
+    public void changeStatus(UserStatus userStatus) {
+        this.userStatus = userStatus;
+    }
 }
