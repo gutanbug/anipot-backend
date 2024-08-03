23068899c0ae41877d14b9668af3179e0315d508
ChaHyeonMin <gutanbug53@gmail.com>
2024-08-03 20:13:07 +0900

    feat : 로깅 엔티티 repository, service 추가

--- src/main/java/org/anipotbackend/global/error/service/impl/ErrorServiceImpl.java
+++ src/main/java/org/anipotbackend/global/error/service/impl/ErrorServiceImpl.java
@@ -1,2 +1,31 @@
-package org.anipotbackend.global.error.service.impl;public class ErrorServiceImpl {
+package org.anipotbackend.global.error.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.anipotbackend.global.auth.jwt.AppAuthentication;
+import org.anipotbackend.global.base.ResponsePage;
+import org.anipotbackend.global.error.model.ErrorSource;
+import org.anipotbackend.global.error.model.dto.response.ResponseErrorInfo;
+import org.anipotbackend.global.error.model.entity.ErrorLogEntity;
+import org.anipotbackend.global.error.repository.ErrorRepository;
+import org.anipotbackend.global.error.service.ErrorService;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ErrorServiceImpl implements ErrorService {
+
+    private final ErrorRepository errorRepository;
+
+    @Override
+    public ResponsePage<ResponseErrorInfo> getAllErrorLog(ErrorSource source, Pageable pageable, AppAuthentication auth) {
+        return null;
+    }
+
+    @Override
+    public void saveErrorLogEntity(ErrorLogEntity entity) {
+        errorRepository.save(entity);
+    }
 }
