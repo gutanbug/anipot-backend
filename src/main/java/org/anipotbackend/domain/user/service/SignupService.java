35b5c151567c947b996e1f293ede6df702fdcdc7
ChaHyeonMin <gutanbug53@gmail.com>
2024-08-04 05:15:21 +0900

    feat : 회원가입 로직 수정 및 로그인 기능 추가

--- src/main/java/org/anipotbackend/domain/user/service/SignupService.java
+++ src/main/java/org/anipotbackend/domain/user/service/SignupService.java
@@ -4,4 +4,11 @@ import org.anipotbackend.domain.user.model.dto.request.UserSignupRequest;
 
 public interface SignupService {
     void signup(UserSignupRequest request);
+    void checkValidPhone(String phone);
+    void checkValidEmail(String email);
+    void checkAlreadyPhone(String phone);
+    void checkAlreadyLoginId(String loginId);
+    void checkAlreadyEmail(String email);
+    void checkValidNickname(String nickname);
+    void checkValidPassword(String password);
 }
